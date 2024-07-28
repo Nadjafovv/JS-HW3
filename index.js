71d5decda31ab7bbbbe5ca0eb41d30a9b9e581c3
Nadjafovv <orxannadjafov29@gmail.com>
2024-07-28 21:44:14 +0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,59 @@
+//task 1
+
+// const porschebtn = document.getElementById(carbtn)
+// const porscheMath = document.getElementById(carMath)
+// const carobj = {
+//     manufacturer: 'Porsche',
+//     model: '911 turbo s',
+//     year: '2013',
+//     averagespeed: '80'
+// }
+
+// carbtn.addEventListener("click", () => {
+//     alert('Марка: ' + carobj.manufacturer)
+//     alert('Модель: ' + carobj.model)
+//     alert('Год: ' + carobj.year)
+//     alert('Средняя скорость: ' + carobj.averagespeed + 'км/ч')
+//   });
+// carMath.addEventListener("click", () => {
+//     let distnum = +prompt('Введите расстояние')
+//     let resttime = (distnum / 5);
+//     let fulltime = ((distnum + resttime) / 100);
+//    alert("За " + fulltime + " часа или часов, опять тимур мяллим играется с кодом 😡") 
+//   });
+
+// task 2
+
+
+// const DrobBtn = document.getElementById(drobbtn1)
+// const plusBtn = document.getElementById(plusMath)
+// const minusrBtn = document.getElementById(minusMath)
+// const multiplyBtn = document.getElementById(multiplyMath)
+// const divideBtn = document.getElementById(divideMath)
+// const reduceBtn = document.getElementById(reduceMath)
+
+// const chislitelObj = {
+//   chis: ""
+// }
+// const znamenatelObj = {
+//   znam: ""
+// }
+// chislitelObj.chis = +prompt ("Введите числитель")
+
+// znamenatelObj.znam = +prompt ("Введите знаменатель")
+
+// drobbtn1.addEventListener("click", () => {
+//   alert(chislitelObj.chis + "/" + znamenatelObj.znam)
+//   });
+// plusMath.addEventListener("click", () => {
+//   alert(chislitelObj.chis + znamenatelObj.znam)
+//   });
+// minusMath.addEventListener("click", () => {
+//   alert(chislitelObj.chis - znamenatelObj.znam)
+//   });
+// multiplyMath.addEventListener("click", () => {
+//   alert(chislitelObj.chis * znamenatelObj.znam)
+//   });
+// divideMath.addEventListener("click", () => {
+//   alert(chislitelObj.chis / znamenatelObj.znam)
+//   });
